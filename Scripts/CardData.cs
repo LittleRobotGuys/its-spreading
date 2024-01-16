aa058a692b276db853e3b0a33b0c26157cdfd779
LittleRobotGuys <little.robot.guys@gmail.com>
2024-01-16 14:26:21 -0500

    Minor edits
    Added a droplet too, more placeholders to come

--- Scripts/CardData.cs
+++ Scripts/CardData.cs
@@ -26,7 +26,7 @@ public partial class CardData : GodotObject
 	public enum CardType { Creature, Action, Enhancement, Environment }
 
 
-    public List<card_base> DATA = new List<card_base>()
+	public List<card_base> DATA = new List<card_base>()
 	{
 		/*
 		 * Failed Vampire / Sick Villager	Creature	2m/10/5d	Attacks randomly each round / Blood counts as negative at end of combat
@@ -36,8 +36,8 @@ public partial class CardData : GodotObject
 		 * Vampire / Vassal	Creature	3m/30/15d	
 		 * Dracula / Abraham Van Helsing	Creature	5m/50/25d	Attacks all units in column in front of him / Van helsing only costs 4 mana splits total damage rounded up to all vampires on the board
 		 */
-        new card_base ("Juvenile Vampire", CardType.Creature, 2, 20, 5, "This newly minted blood sucker is a mainstay in your spreading army"),
-        new card_base ("Militia", CardType.Creature, 2, 20, 5, "I hate vampires so hard!"),
+		new card_base ("Juvenile Vampire", CardType.Creature, 2, 20, 5, "This newly minted blood sucker is a mainstay in your spreading army"),
+		new card_base ("Militia", CardType.Creature, 2, 20, 5, "I hate vampires so hard!"),
 		new card_base ("Bat", CardType.Creature, 1, 10, 2, "Squeak!"),
 		new card_base ("Hound", CardType.Creature, 1, 10, 2, "Arf!"),
 		new card_base ("Vampire", CardType.Creature, 3, 30, 15, "This grotesque monster is stronger than the juveniles"),
@@ -59,15 +59,15 @@ public partial class CardData : GodotObject
 		 */
 		
 		new card_base ("Vampiric Ritual", CardType.Action, 3, 0, 0,   "Consume 2 lesser vampires to create 1 greater vampire"),
-        new card_base ("Blood letting", CardType.Action, 2, 0, 0,     "Heal up to 10 missing blood from any unit on the battlefield"),
-        new card_base ("Backstab", CardType.Action, 1, 0, 0,          "Deal 5 damage directly to any unit on the battlefield (double damage if the tile behind them in unoccupied)"),
-        new card_base ("Wooden Stake", CardType.Action, 1, 0, 0,      "Deal 5 to any vampire"),
-        new card_base ("Tactical Maneuver", CardType.Action, 2, 0, 0, "Move 3 units you control to different spaces on the battlefield"),
-        new card_base ("Spread Disease", CardType.Action, 3, 0, 0,    "Turn all Villagers on the battlefield SICK"),
-        new card_base ("Sacrifice", CardType.Action, 2, 0, 0,         "Target 3x3 grid alll units inside take 10 damage"),
-        new card_base ("Press on", CardType.Action, 1, 0, 0,          "Advance all friendly units who are able 1 space"),
-        new card_base ("Inspire Fear", CardType.Action, 2, 0, 0,      "Increase all friendly units attack by 5 until end of turn"),
-        new card_base ("To Arms!", CardType.Action, 3, 0, 0,          "Consume 3 villagers to produce 1 Militia"),
+		new card_base ("Blood letting", CardType.Action, 2, 0, 0,     "Heal up to 10 missing blood from any unit on the battlefield"),
+		new card_base ("Backstab", CardType.Action, 1, 0, 0,          "Deal 5 damage directly to any unit on the battlefield (double damage if the tile behind them in unoccupied)"),
+		new card_base ("Wooden Stake", CardType.Action, 1, 0, 0,      "Deal 5 to any vampire"),
+		new card_base ("Tactical Maneuver", CardType.Action, 2, 0, 0, "Move 3 units you control to different spaces on the battlefield"),
+		new card_base ("Spread Disease", CardType.Action, 3, 0, 0,    "Turn all Villagers on the battlefield SICK"),
+		new card_base ("Sacrifice", CardType.Action, 2, 0, 0,         "Target 3x3 grid alll units inside take 10 damage"),
+		new card_base ("Press on", CardType.Action, 1, 0, 0,          "Advance all friendly units who are able 1 space"),
+		new card_base ("Inspire Fear", CardType.Action, 2, 0, 0,      "Increase all friendly units attack by 5 until end of turn"),
+		new card_base ("To Arms!", CardType.Action, 3, 0, 0,          "Consume 3 villagers to produce 1 Militia"),
 
-    };
+	};
 }
--- Scripts/card_base.cs
+++ Scripts/card_base.cs
@@ -5,29 +5,54 @@ using static CardData;
 public partial class card_base : MarginContainer
 {
 	string DefaultCardInfo = "UNINITIALIZED CARD INFO";
-    string CardInfo;
-    string CardFlavorText;
+	string CardInfo;
+	string CardFlavorText;
 	Image CardImage = null;
-    private string name;
-    private CardType type;
-    private int cost;
-    private int blood;
-    private int attack;
+	private string name;
+	private CardType type;
+	private int cost;
+	private int blood;
+	private int attack;
 
-    public card_base(string name, CardType type, int cost, int blood, int attack, string cardInfo, string flavorText = "")
-    {
-        this.name = name;
-        this.type = type;
-        this.cost = cost;
-        this.blood = blood;
-        this.attack = attack;
-        this.CardInfo = cardInfo == null ? cardInfo : DefaultCardInfo;
-        this.CardFlavorText = flavorText;
-    }
+	private int borderWidth = 5;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	public card_base(string name, CardType type, int cost, int blood, int attack, string cardInfo, string flavorText = "")
 	{
+		this.name = name;
+		this.type = type;
+		this.cost = cost;
+		this.blood = blood;
+		this.attack = attack;
+		this.CardInfo = cardInfo == null ? cardInfo : DefaultCardInfo;
+		this.CardFlavorText = flavorText;
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Created a card named " + this.name + "!");
+
+		switch (type)
+		{
+			case CardType.Action:
+				break;
+			case CardType.Creature:
+				break;
+			case CardType.Enhancement:
+				break;
+			case CardType.Environment:
+				break;
+			default:
+				break;
+
+		}
+
+
+
+		AddThemeConstantOverride("margin_top", borderWidth);
+		AddThemeConstantOverride("margin_left", borderWidth);
+		AddThemeConstantOverride("margin_bottom", borderWidth);
+		AddThemeConstantOverride("margin_right", borderWidth);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
