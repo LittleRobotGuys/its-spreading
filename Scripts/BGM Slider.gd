6964207710cdf73951f2a8a618798ebf65ad94d7
TypeOGojirra <145849438+TypeOGojirra@users.noreply.github.com>
2024-01-18 16:33:54 -0500

    Added load game scene & fixed some bugs

--- Scripts/BGM Slider.gd
+++ Scripts/BGM Slider.gd
@@ -14,3 +14,6 @@ func _on_value_changed(value: float) -> void:
 
 
 
+
+
+
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -6,12 +6,12 @@ extends Control
 @onready var options_button = $HBoxContainer/VBoxContainer/options_button as Button
 @onready var quit_button = $HBoxContainer/VBoxContainer/quit_button as Button
 @onready var menu_margin_container = $"." as MarginContainer
-@onready var options_menu_container = $"../options_menu_container" as MarginContainer
-@onready var options_back_button = $"../options_menu_container/VBoxContainer/back_button" as Button
+@onready var options_menu_container =  $"../options_menu" as VBoxContainer
+@onready var options_back_button = $"../options_menu/back_button" as Button
 @onready var new_game_scene = preload("res://Scenes/new_game.tscn") as PackedScene
 @onready var fullscreen_button = $"../options_menu_container/VBoxContainer/HBoxContainer/fullscreen_button" as Button
 @onready var windowed_button = $"../options_menu_container/VBoxContainer/HBoxContainer/windowed_button" as Button
-
+@onready var load_menu = $"../load_menu" as Node
 
 func _ready():
 	handle_connecting_signals()
@@ -19,23 +19,27 @@ func _ready():
 func on_new_game_button_press():
 	get_tree().change_scene_to_packed(new_game_scene)
 
-#func on_load_button_press():
-
+func _on_load_button_pressed() -> void:
+	load_menu.visible = true
+	menu_margin_container.visible = false
+	options_menu_container.visible = false
 
 func on_options_button_press():
 	menu_margin_container.visible = false
 	options_menu_container.visible = true
+	load_menu.visible = false
 
 func on_options_back_button_press():
 	menu_margin_container.visible = true
 	options_menu_container.visible = false
+	load_menu.visible = false
 
 func on_quit_button_press():
 	get_tree().quit()
 
 func handle_connecting_signals():
 	new_game_button.button_down.connect(on_new_game_button_press)
-	#load_button.button_down.connect(on_load_button_press)
+	#load_button.button_down.connect()
 	options_button.button_down.connect(on_options_button_press)
 	quit_button.button_down.connect(on_quit_button_press)
 	options_back_button.button_down.connect(on_options_back_button_press)
@@ -45,3 +49,8 @@ func _on_windowed_button_pressed() -> void:
 
 func _on_fullscreen_button_pressed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+
+func _on_load_back_button_pressed() -> void:
+	menu_margin_container.visible = true
+	options_menu_container.visible = false
+	load_menu.visible = false
