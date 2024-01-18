032d879e94f30fd8d1975b4283bec9b00724b1bb
Kwatters7 <151787003+Kwatters7@users.noreply.github.com>
2024-01-18 15:53:51 -0500

    Add files via upload

---
+++ eternalValev1/scripts/drawRectangle.gd
@@ -0,0 +1,13 @@
+
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_draw()
+	
+	pass
+
+func _draw():
+	draw_rect(Rect2(0,0,1500,650), Color.BISQUE, true)
+	
--- eternalValev1/scripts/mainMap.gd
+++ eternalValev1/scripts/mainMap.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 @onready var to_battle = preload("res://Scenes/battle.tscn")
 @onready var to_battle1 = preload('res://Scenes/battle1.tscn')
+@onready var to_battle2 = preload('res://Scenes/battle2.tscn')
+@onready var to_shop = preload("res://scenes/shop.tscn")
+@onready var to_rest = preload("res://scenes/rest.tscn")
+@onready var to_boss = preload("res://scenes/bossBattle.tscn")
 
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(to_battle)
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_packed(to_battle1)
+	
+func _on_button_3_pressed():
+	get_tree().change_scene_to_packed(to_battle2)
