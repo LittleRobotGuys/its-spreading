4065f4de944e70a92dbd0e40c5d27283def8e6c8
Kwatters7 <151787003+Kwatters7@users.noreply.github.com>
2024-01-19 15:49:50 -0500

    Add files via upload

--- eternalValev1/scripts/drawBattle.gd
+++ eternalValev1/scripts/drawBattle.gd
@@ -1,4 +1,10 @@
 extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_draw()
+	pass
+
+func _draw():
+	draw_rect(Rect2(0,0,1500,650), Color.DARK_SLATE_GRAY, true)
 	
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/mainMap(git).tscn")
--- eternalValev1/scripts/mainMap.gd
+++ eternalValev1/scripts/mainMap.gd
@@ -1,8 +1,8 @@
 extends Node2D
-#
-@onready var to_battle1 = preload("res://Scenes/battle.tscn")
-@onready var to_battle1_1 = preload('res://Scenes/battle1.tscn')
-@onready var to_battle1_2 = preload('res://Scenes/battle2.tscn')
+
+@onready var to_battle = preload("res://Scenes/battle.tscn")
+@onready var to_battle1_1 = preload("res://Scenes/battle1.tscn")
+@onready var to_battle1_2 = preload("res://Scenes/battle2.tscn")
 @onready var to_shop = preload("res://scenes/shop.tscn")
 @onready var to_rest = preload("res://scenes/rest.tscn")
 @onready var to_boss = preload("res://scenes/bossBattle.tscn")
@@ -10,16 +10,19 @@ extends Node2D
 @onready var to_mystery_2_2 = preload("res://scenes/mystery2_2.tscn")
 @onready var to_shop_3_1 = preload("res://scenes/shop3_1.tscn")
 @onready var to_elite_battle_3_2 = preload("res://scripts/eliteBattle3_2.tscn")
+@onready var to_battle_3_3 = preload("res://scenes/battle3_3.tscn")
+
 
 #Top 3 functions for row 1 encounters
-func _on_battle_1_1_pressed():
-	get_tree().change_scene_to_packed(to_battle1)
+func _on_button_1_pressed():
+	get_tree().change_scene_to_packed(to_battle)
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_packed(to_battle1_1)
 	
 func _on_button_3_pressed():
 	get_tree().change_scene_to_packed(to_battle1_2)
+	
 # Next 2 functions for row 2 encounters
 func _on_battle_2_1_pressed():
 	get_tree().change_scene_to_packed(to_battle2_1)
@@ -33,3 +36,6 @@ func _on_shop_3_1_pressed():
 	
 func _on_elite_battle_3_2_pressed():
 	get_tree().change_scene_to_packed(to_elite_battle_3_2)
+
+func _on_battle_3_3_pressed():
+	get_tree().change_scene_to_packed(to_battle_3_3)
